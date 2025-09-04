c6eb6af117b2947403e17c88ab9be9dabc2e6359
atharvapatil22 <atharva.22.patil@gmail.com>
2025-09-04 00:08:41 -0400

    updated shared code and docker configs

--- shared/constants.py
+++ shared/constants.py
@@ -8,7 +8,7 @@ WORKLOAD_STATUSES = {
   "searching_comics" : "SEARCHING_EXISTING_COMICS",
   "awaiting_user_choice": "AWAITING_USER_CHOICE",
   "generating_prompts" : "GENERATING_PROMPTS",
-  "generating images" : "GENERATING_IMAGES",
+  "generating_images" : "GENERATING_IMAGES",
   "styling_images" : "STYLING_IMAGES",
   "merging_comic_pages" : "MERGING_COMIC_PAGES",
   "completed_w_existing" :"COMPLETED_W_EXISTING",
@@ -17,5 +17,20 @@ WORKLOAD_STATUSES = {
   "failed_overlimit" : "FAILED_OVERLIMIT",
 }
 
-# 2] For workers- Image generation constants
+# 2] For workers
+
+# Image generation constants
 IMG_GEN_LIMIT = 20
+ING_IMAGE_SIZE = "1024x1024"
+INS_IMAGE_SIZE = "1792x1024"
+POSTER_IMAGE_SIZE = "1024x1792"
+
+# Rate Limit constants (RL)
+RL_DALLEE_BATCH_SIZE = 5 #How many parallel calls(batch size) to the Image generation api
+RL_DALEE_WAIT_TIME = 60 #How many seconds to wait, before next batch of calls to Image generation api
+
+# Page Styling constants (PS)
+FINAL_PAGE_HEIGHT = 1792
+FINAL_PAGE_WIDTH = 1024
+
+PS_TITLE_HEIGHT = 140
